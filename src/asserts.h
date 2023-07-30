8741fd959b0f00720d59e70c9cb5730d97998fd7
Falko Schumann <falko.schumann@muspellheim.de>
2023-07-30 11:17:19 +0200

    feat: add more assertations

--- src/asserts.h
+++ src/asserts.h
@@ -9,10 +9,87 @@
 #ifndef ASSERTS_H
 #define ASSERTS_H
 
+#include <math.h>
+#include <string.h>
+
 #define ASSERT_TRUE(value)                                            \
   {                                                                   \
     assert_implementation((value), ("expected: true but was: false"), \
                           __FILE__, __LINE__);                        \
+  }
+
+#define ASSERT_FALSE(value)                                            \
+  {                                                                    \
+    assert_implementation(!(value), ("expected: false but was: true"), \
+                          __FILE__, __LINE__);                         \
+  }
+
+#define ASSERT_EQUALS(expected, actual)                                  \
+  {                                                                      \
+    assert_implementation((expected == actual),                          \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_NOT_EQUALS(expected, actual)                              \
+  {                                                                      \
+    assert_implementation((expected != actual),                          \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_DOUBLE_EQUALS(expected, actual, delta)                    \
+  {                                                                      \
+    assert_implementation((fabs(expected - actual) < delta),             \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_DOUBLE_NOT_EQUALS(expected, actual, delta)                \
+  {                                                                      \
+    assert_implementation((fabs(expected - actual) >= delta),            \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_STRING_EQUALS(expected, actual)                           \
+  {                                                                      \
+    assert_implementation((strcmp(expected, actual) == 0),               \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_STRING_NOT_EQUALS(expected, actual)                       \
+  {                                                                      \
+    assert_implementation((strcmp(expected, actual) != 0),               \
+                          ("expected: " #expected " but was: " #actual), \
+                          __FILE__, __LINE__);                           \
+  }
+
+#define ASSERT_NULL(value)                                                 \
+  {                                                                        \
+    assert_implementation((NULL == (const void *)(value)),                 \
+                          ("expected: null pointer"), __FILE__, __LINE__); \
+  }
+
+#define ASSERT_NOT_NULL(value)                                                 \
+  {                                                                            \
+    assert_implementation((NULL != (const void *)(value)),                     \
+                          ("expected: non null pointer"), __FILE__, __LINE__); \
+  }
+
+#define ASSERT_SAME(expected, actual)                         \
+  {                                                           \
+    assert_implementation(                                    \
+        ((const void *)(expected) == (const void *)(actual)), \
+        ("expected: same pointer"), __FILE__, __LINE__);      \
+  }
+
+#define ASSERT_NOT_SAME(expected, actual)                     \
+  {                                                           \
+    assert_implementation(                                    \
+        ((const void *)(expected) != (const void *)(actual)), \
+        ("expected: not same pointer"), __FILE__, __LINE__);  \
   }
 
 /* Prints a summary of the test suite.
--- test/testsuite.c
+++ test/testsuite.c
@@ -6,24 +6,87 @@
  * Released under the terms of the MIT License.
  */
 
+#include <assert.h>
+
 #include "../src/asserts.h"
 
-static unsigned int expected_failures = 0;
-static unsigned int failure_count;
+void run_successful(void) {
+  unsigned int expected_failures = 0;
+  unsigned int failure_count;
 
-void run_successful(void) { ASSERT_TRUE(1 == 1); }
+  char *pointer = "foo";
+  char *same_pointer = pointer;
+  char *other_pointer = "bar";
+  char *null_pointer = 0;
+
+  ASSERT_TRUE(1 == 1);
+  ASSERT_FALSE(1 == 2);
+  ASSERT_EQUALS(1, 1);
+  ASSERT_NOT_EQUALS(1, 2);
+  ASSERT_DOUBLE_EQUALS(1.0, 1.05, 0.1);
+  ASSERT_DOUBLE_NOT_EQUALS(1.0, 1.1, 0.1);
+  ASSERT_STRING_EQUALS("foobar", "foobar");
+  ASSERT_STRING_NOT_EQUALS("foo", "bar");
+  ASSERT_NULL(null_pointer);
+  ASSERT_NOT_NULL(pointer);
+  ASSERT_SAME(pointer, same_pointer);
+  ASSERT_NOT_SAME(pointer, other_pointer);
+
+  failure_count = print_summary();
+  assert(failure_count == expected_failures);
+}
 
 void run_failing(void) {
+  unsigned int expected_failures = 0;
+  unsigned int failure_count;
+
+  char *pointer = "foo";
+  char *same_pointer = pointer;
+  char *other_pointer = "bar";
+  char *null_pointer = 0;
+
   ASSERT_TRUE(1 == 2);
   expected_failures++;
+
+  ASSERT_FALSE(1 == 1);
+  expected_failures++;
+
+  ASSERT_EQUALS(1, 2);
+  expected_failures++;
+
+  ASSERT_NOT_EQUALS(1, 1);
+  expected_failures++;
+
+  ASSERT_DOUBLE_EQUALS(1.0, 1.1, 0.1);
+  expected_failures++;
+
+  ASSERT_DOUBLE_NOT_EQUALS(1.0, 1.05, 0.1);
+  expected_failures++;
+
+  ASSERT_STRING_EQUALS("foo", "bar");
+  expected_failures++;
+
+  ASSERT_STRING_NOT_EQUALS("foobar", "foobar");
+  expected_failures++;
+
+  ASSERT_NULL(pointer);
+  expected_failures++;
+
+  ASSERT_NOT_NULL(null_pointer);
+  expected_failures++;
+
+  ASSERT_SAME(pointer, other_pointer);
+  expected_failures++;
+
+  ASSERT_NOT_SAME(pointer, same_pointer);
+  expected_failures++;
+
+  failure_count = print_summary();
+  assert(failure_count == expected_failures);
 }
 
 int main(void) {
-  int ok;
   run_successful();
   run_failing();
-
-  failure_count = print_summary();
-  ok = failure_count - expected_failures;
-  return ok;
+  return 0;
 }
